ffc637c81f2fb29734833f20435bc18cdda5b4c5
Budi Prakosa <iyok@deadmediafm.org>
2017-07-16 19:52:54 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,113 @@
+window.addEventListener('load', init, false);
+function init() {
+
+    var hexdata = [14,192,29,192,28,192,27,192,26,192,49,192,24,192,23,192,22,192,21,192,20,192,19,192,18,192,17,192,16,192,17,36,31,190,207,229,210,224,222,191,205,191,32,224,160,230,176,224,1,192,29,146,169,54,178,7,225,247,4,208,119,192,224,207,8,149,8,149,129,183,129,191,92,208,250,223,250,223,254,207,128,183,128,127,128,191,128,183,128,104,128,191,140,181,128,100,140,189,143,239,141,189,128,183,135,96,128,191,8,149,31,146,15,146,15,182,15,146,17,36,47,147,63,147,143,147,159,147,175,147,191,147,128,145,97,0,144,145,98,0,160,145,99,0,176,145,100,0,48,145,96,0,38,224,35,15,45,55,48,240,41,232,35,15,3,150,161,29,177,29,3,192,2,150,161,29,177,29,32,147,96,0,128,147,97,0,144,147,98,0,160,147,99,0,176,147,100,0,128,145,101,0,144,145,102,0,160,145,103,0,176,145,104,0,1,150,161,29,177,29,128,147,101,0,144,147,102,0,160,147,103,0,176,147,104,0,191,145,175,145,159,145,143,145,63,145,47,145,15,144,15,190,15,144,31,144,24,149,138,181,130,96,138,189,138,181,129,96,138,189,131,183,136,127,131,96,131,191,120,148,137,183,130,96,137,191,152,223,134,177,136,119,134,104,134,185,55,154,8,149,248,148,255,207];
+    
+    var hex_string = "";
+
+    //--webaudio
+    var audioCtx;
+    try {
+        window.AudioContext= window.AudioContext||window.webkitAudioContext;
+        audioCtx= new AudioContext();
+    }
+    catch(e) {
+        alert('Web Audio API is not supported in this browser');
+    }
+
+    var wg = new WavCodeGenerator();
+
+
+    //--hex data input
+    document.querySelector('#hexfileInput').onchange= function(e) {
+        var file= e.target.files[0];
+        if(file) {
+            var reader= new FileReader();
+            reader.onload= function(ee) {
+                var hexData= ee.target.result;
+                hex_string= hexData;
+                //console.log('name: '+file.name+'\n'+'type: '+file.type+'\n'+'size: '+file.size+'\n'+'contents: '+hexData);
+                if(hexData[0]===':') { //TODO also check min/max filesize?
+                    document.querySelector('#hexRaw').value= hexData;
+                    var event= new Event('change');
+                    document.querySelector('#hexRaw').dispatchEvent(event);
+                } else {
+                    alert('Failed to load hex file - not a proper hex file');
+                }
+            }
+            reader.readAsText(file);
+        } else {
+            alert('Failed to load hex file');
+        }
+    }
+
+    //--hex decoded data
+    document.querySelector('#hexRaw').onchange= function(e) {
+        document.querySelector('#hexDecoded').value= hex_string;
+    }
+    
+
+    document.querySelector('#playButton').onclick= function() {
+
+        var decoded_hex_array = decodeHexFile(hex_string);
+        var signal = wg.generateSignal(decoded_hex_array);
+        wg.playSignal(audioCtx, signal);
+
+        console.log("sig ", signal.length);
+
+        //debug
+        var res_sig = "";
+        for (var i = 0; i < signal.length; i++) {
+            res_sig += signal[i] + "\n";
+        }
+        document.querySelector('#hexDecoded').value= res_sig;
+
+
+
+    }
+
+
+    var sendControl = function(arr)
+    {
+        var sig = wg.generateControlSignal(arr);        
+        wg.playSignal(audioCtx, sig);
+
+        //debug
+        var res_sig = "";
+        for (var i = 0; i < sig.length; i++) {
+            res_sig += sig[i] + "\n";
+        }
+        document.querySelector('#hexDecoded').value= res_sig;
+
+    }
+
+    document.querySelector('#controlVar1').onchange= function(e) {
+
+        var var1 = parseInt(document.querySelector('#controlVar1').value);
+        var var2 = parseInt(document.querySelector('#controlVar2').value);
+
+        sendControl([var1, var2]);
+
+
+    }
+
+    document.querySelector('#controlVar2').onchange= function(e) {
+
+        var var1 = parseInt(document.querySelector('#controlVar1').value);
+        var var2 = parseInt(document.querySelector('#controlVar2').value);
+
+        sendControl([var1, var2]);
+
+
+    }
+    
+    document.querySelector('#sendControlSignal').onclick= function() {
+
+        var var1 = parseInt(document.querySelector('#controlVar1').value);
+        var var2 = parseInt(document.querySelector('#controlVar2').value);
+
+        sendControl([var1, var2]);
+
+
+    }
+}
