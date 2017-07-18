e0a2d3472f015282cb8922cf713b4b2b267b14a9
Budi Prakosa <iyok@deadmediafm.org>
2017-07-18 19:41:40 +0700

    refactor

--- js/main.js
+++ js/main.js
@@ -5,6 +5,28 @@ function init() {
     var hexdata = [14,192,29,192,28,192,27,192,26,192,49,192,24,192,23,192,22,192,21,192,20,192,19,192,18,192,17,192,16,192,17,36,31,190,207,229,210,224,222,191,205,191,32,224,160,230,176,224,1,192,29,146,169,54,178,7,225,247,4,208,119,192,224,207,8,149,8,149,129,183,129,191,92,208,250,223,250,223,254,207,128,183,128,127,128,191,128,183,128,104,128,191,140,181,128,100,140,189,143,239,141,189,128,183,135,96,128,191,8,149,31,146,15,146,15,182,15,146,17,36,47,147,63,147,143,147,159,147,175,147,191,147,128,145,97,0,144,145,98,0,160,145,99,0,176,145,100,0,48,145,96,0,38,224,35,15,45,55,48,240,41,232,35,15,3,150,161,29,177,29,3,192,2,150,161,29,177,29,32,147,96,0,128,147,97,0,144,147,98,0,160,147,99,0,176,147,100,0,128,145,101,0,144,145,102,0,160,145,103,0,176,145,104,0,1,150,161,29,177,29,128,147,101,0,144,147,102,0,160,147,103,0,176,147,104,0,191,145,175,145,159,145,143,145,63,145,47,145,15,144,15,190,15,144,31,144,24,149,138,181,130,96,138,189,138,181,129,96,138,189,131,183,136,127,131,96,131,191,120,148,137,183,130,96,137,191,152,223,134,177,136,119,134,104,134,185,55,154,8,149,248,148,255,207];    
     var hex_string = "";
 
+
+
+    var sendControl = function(arr)
+    {
+        var sig = hex2wav.generateControlSignal(arr);        
+        hex2wav.playSignal(hex2wav.audioCtx, sig);
+
+        //debug
+        var res_sig = "";
+        for (var i = 0; i < sig.length; i++) {
+            res_sig += sig[i] + "\n";
+        }
+        document.querySelector('#hexDecoded').value= res_sig;
+
+    }
+
+    var sendToEEPROM = function (arr)
+    {
+        var signal = hex2wav.generateEEPROMSignal(arr);
+        hex2wav.playSignal(hex2wav.audioCtx, signal);
+    }
+
     //--hex data input
     document.querySelector('#hexfileInput').onchange= function(e) {
         var file= e.target.files[0];
@@ -51,27 +73,6 @@ function init() {
 
 
 
-    }
-
-
-    var sendControl = function(arr)
-    {
-        var sig = hex2wav.generateControlSignal(arr);        
-        hex2wav.playSignal(hex2wav.audioCtx, sig);
-
-        //debug
-        var res_sig = "";
-        for (var i = 0; i < sig.length; i++) {
-            res_sig += sig[i] + "\n";
-        }
-        document.querySelector('#hexDecoded').value= res_sig;
-
-    }
-
-    var sendToEEPROM = function (arr)
-    {
-        var signal = hex2wav.generateEEPROMSignal(arr);
-        hex2wav.playSignal(hex2wav.audioCtx, signal);
     }
 
     document.querySelector('#controlVar1').onchange= function(e) {
